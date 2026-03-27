1daa84d1849d64553905a8d1402539be2780b0c3
Amanda Allred <amanda.barnett11@yahoo.com>
2026-03-26 21:24:18 -0400

    added for loop reversal

--- reverse.py
+++ reverse.py
@@ -1,7 +1,23 @@
-def reverse_string(string):
-    return string[-1::-1]
+# ''' 
+# Reverse a string using the slice method
+# '''
 
+# def reverse_string(string):
+#     return string[-1::-1]
 
-print(reverse_string("hello"))
-print(reverse_string("world"))
-print(reverse_string("hello world"))
+# print(reverse_string("hello"))
+# print(reverse_string("world"))
+# print(reverse_string("hello world"))
+
+''' 
+Reverse a string manually using `for` loop
+Does this count as manually?
+'''
+
+def reverse_string_loop(string):
+    new_string = ''
+    for char in reversed(string):
+        new_string += char
+    return new_string
+
+print(reverse_string_loop("HELLO"))
